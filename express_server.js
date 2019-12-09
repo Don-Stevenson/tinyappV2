e3c614658b7a505415bd0f9e1a4df8b239956873
Don-Stevenson <dsteven1@gmail.com>
2019-12-09 18:43:50 +0000

    added the hello html and the urls json page

--- express_server.js
+++ express_server.js
@@ -8,10 +8,23 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+// Gets
+// *************
 // Initial setup for the homepage of tinyApp
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
+
+// page that displays the urls as a json object
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+// page that displays a basic hello html page
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
 
 // Response to the command line that shows the server is up an running
 app.listen(PORT, () => {
