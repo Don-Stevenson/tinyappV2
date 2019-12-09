e4bebee2ec7843a5c1b5f816293e8743fdf3a734
Don-Stevenson <dsteven1@gmail.com>
2019-12-09 18:37:41 +0000

    commented the functionality of the code

--- express_server.js
+++ express_server.js
@@ -2,15 +2,18 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+// database containing the urls for tinyApp
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+// Initial setup for the homepage of tinyApp
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+// Response to the command line that shows the server is up an running
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
