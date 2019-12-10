ca3feef600f7853d0076cd7c2b7abaf244edcf8b
Don-Stevenson <dsteven1@gmail.com>
2019-12-10 00:07:57 +0000

    short and long URL are created and longURL redirects properly

--- express_server.js
+++ express_server.js
@@ -10,13 +10,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // DATABASE
 // database containing the urls for tinyApp
+//*******************************************
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
 // Gets
-// *************
+// ******************************************
 // Initial setup for the homepage of tinyApp
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -32,37 +33,54 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+// route that renders the short url 
+app.get("/urls/:shortURL", (req, res) => {
+  let templateVars = { 
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL]
+  };
+  console.log(templateVars)
+  res.render("urls_show", templateVars);
+});
+
 // route that renders the urls from urls_index
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
-// route that renders the short url 
-app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
-  res.render("urls_show", templateVars);
-});
-
-
 // page that displays a basic hello html page
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+//redirect any request from a short url to its long URL
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+
+  res.redirect(longURL);
+});
+
+
+
+//here problems
+
 //Posts
-//********************
+//**********************************************
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  const randomURL = generateRandomString();
+  urlDatabase[randomURL] = req.body.longURL;
+  // let templateVars = {shortURL: randomURL, longURL: req.body.longURL}
+
+  // console.log(urlDatabase);
+  res.redirect("/urls/" + randomURL);
 });
 
 
 // Function that generates a random string for shortening a url 
-function generateRandomString() {
+const generateRandomString = () => {
   let result = "";
-  let chars = "abcdefghijklmnopqurstuvwxyz";
-  chars += "0123456789";
+  let chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqurstuvwxyz0123456789";
   for (let i = 0; i < 6; i++) {
     result += chars.charAt(Math.floor(Math.random() * chars.length));
   }
@@ -71,7 +89,7 @@ function generateRandomString() {
 
 
 //Server up and listening
-//***********************
+//**********************************************
 // Response to the command line that shows the server is up an running
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
